0c86ceac1e7f47548f9d9046d089ba1cc1a44805
Millare Jr <w.a.millare.jr@accenture.com>
2018-05-07 13:59:00 +0800

    added Jay's portal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
-import { GoalDetailComponent } from './goal-detail/goal-detail.component';
-import { GoalListComponent } from './goal-list/goal-list.component';
-import { GoalPrimaryComponent } from './goal-primary/goal-primary.component';
-import { GoalSecondaryComponent } from './goal-secondary/goal-secondary.component';
+import { JayMillarePortalComponent } from './portals/jay-millare-portal/jay-millare-portal.component';
+
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RouterModule, Routes } from '@angular/router';
+import { GoalListComponent } from './goals/goal-list/goal-list.component';
+import { GoalDetailComponent } from './goals/goal-detail/goal-detail.component';
+import { GoalPrimaryComponent } from './goals/goal-primary/goal-primary.component';
+import { GoalSecondaryComponent } from './goals/goal-secondary/goal-secondary.component';
 
 const routes: Routes = [
   // default route
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'goals', component: GoalListComponent },
+  { path: 'jaymillare', component: JayMillarePortalComponent },
   {
     path: 'goals/:id',
     component: GoalDetailComponent,
@@ -41,5 +44,7 @@ export const routingComponents = [
   GoalDetailComponent,
   GoalPrimaryComponent,
   GoalSecondaryComponent,
-  PageNotFoundComponent
+  PageNotFoundComponent,
+  // portals
+  JayMillarePortalComponent
 ];
